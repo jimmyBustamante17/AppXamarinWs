0913c183462f1a1a9c76751eccf828d32e86e70a
Jimmy <automa.ingindustrial@prebel.com.co>
2023-03-31 12:46:58 -0500

    Agregar archivos de proyecto.

---
+++ MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace AppXamarinWs
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            EditText txtId = FindViewById<EditText>(Resource.Id.txtId);
+            EditText txtName = FindViewById<EditText>(Resource.Id.txtName);
+            EditText textDescription = FindViewById<EditText>(Resource.Id.txtDescription);
+
+            string urlServicio = "https://jsonplaceholder.typicode.com/posts";
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
